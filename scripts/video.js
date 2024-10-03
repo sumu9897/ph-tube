45262212d44b56424fdf409fb392f04ddd854b6e
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-03 13:20:28 +0600

    Verified function done

--- scripts/video.js
+++ scripts/video.js
@@ -41,7 +41,9 @@ const displayVideos = (videos) =>{
         <h2 class="font-bold">${video.title}</h2>
         <div class="flex items-center gap-2">
             <p class="text-gray-400">${video.authors[0].profile_name}</p>
-            <img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />
+            ${video.authors[0].verified === true ? `            <img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />
+` :" "}
+
 
         </div>
         
---
+++ test.js
@@ -0,0 +1,7 @@
+const isVerified = true;
+
+// if(isVerified === true){
+//     console.log("The user is verified");
+// } else{
+//     console.log("Not")
+// }
