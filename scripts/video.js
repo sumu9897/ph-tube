86b248eb0aba6e1c26d25f5a07286641edaf1fc2
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-03 13:44:13 +0600

    Minutes second convertion

--- scripts/video.js
+++ scripts/video.js
@@ -1,5 +1,13 @@
 
+function getTimeString(time){
 
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+
+    return `${hour} hour ${minute} minutes  ago`;
+}
 const loadCategories = () =>{
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then( res => res.json())
@@ -32,7 +40,11 @@ const displayVideos = (videos) =>{
             <img
             src= ${video.thumbnail}
             alt="Shoes" />
-            <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${video.others.posted_date}</span>
+
+            ${
+                video.others.posted_date?.length == 0 ? "":`<span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)}</span>`
+            }
+            
         </figure>
         <div class="px-0 py-2 flex gap-2">
         <div>
--- test.js
+++
@@ -1,7 +0,0 @@
-const isVerified = true;
-
-// if(isVerified === true){
-//     console.log("The user is verified");
-// } else{
-//     console.log("Not")
-// }
